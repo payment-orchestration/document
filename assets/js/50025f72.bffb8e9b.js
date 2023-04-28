3e9be74baaaf3ad69195e5244e4f6b4db94c2a88
yichaulin <yichaulin@gmail.com>
2023-04-28 21:33:39 +0800

    Deploy website - based on 8992561f0a756106a28e9240c024c38c983e77fb

---
+++ assets/js/50025f72.bffb8e9b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkflexirails=self.webpackChunkflexirails||[]).push([[8016],{2220:l=>{l.exports=JSON.parse('{"label":"hello","permalink":"/document/blog/tags/hello","allTagsPath":"/document/blog/tags","count":2}')}}]);
